1a73e11400838b9e5546b13652425adaae352372
mark_yahorau <mark_yahorau@senla.eu>
2023-04-20 20:14:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/gradleapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.gradleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gradleapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
